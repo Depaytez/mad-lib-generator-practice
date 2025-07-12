f57eb041dbcc3b7db57d0c5f02f5132a5d9a05fb
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-07-12 04:41:04 +0100

    Add age validation using a while loop to ensure user is at least 18 years old

---
+++ usewhile.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+while age < 18:
+ age = int(input("You must be at least 18 years old. Please enter your age again: "))
+print("Thank you! You are old enough to proceed.")
+
+#print a the age of the user
+print(f"You are {age} years old.")
