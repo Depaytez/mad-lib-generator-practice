4f64ca3917ed23c7d3aeefced6766d3db14a385e
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-07-12 03:57:25 +0100

    Add script to print even numbers and their total count

---
+++ evennumbers.py
@@ -0,0 +1,7 @@
+even_set = range(2, 10, 2) 
+for even in even_set:
+ print(f"{even} ")
+
+# After the loop, print the total number of even numbers in the range
+total_even_numbers = len(even_set)
+print(f"Total number of even numbers in the range: {total_even_numbers}")
